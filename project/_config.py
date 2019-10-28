e6c402a517e98749e1cd2d31e8877b49acd97755
cmattheson6 <cmattheson6@gmail.com>
2019-10-28 08:11:51 -0400

    resolve all merge conflicts

--- project/_config.py
+++ project/_config.py
@@ -6,11 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'thisisthelongestkeyever'
-<<<<<<< HEAD
 DEBUG = False
-=======
-DEBUG = True
->>>>>>> fix_master_error
 
 DATABASE_PATH = os.path.join(basedir, DATABASE)
 
