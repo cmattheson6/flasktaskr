c6d025ed34385bfde113427dd0e3bed5081449ac
cmattheson6 <cmattheson6@gmail.com>
2019-11-03 22:43:07 -0500

    testing this

--- tasks.py
+++ tasks.py
@@ -12,14 +12,15 @@ with Connection('localhost') as conn:
     @task
     def commit(c):
         message = input('Enter a git commit message: ')
-        c.run('git add . && git commit -am \'{}\''.format(message))
+        print(type(message))
+        c.run(f'git add . && git commit -m "{message}"')#.format(message))
 
     @task
     def push(c):
-        c.run('git push origin master')
+        c.run('git push origin master -v')
 
     @task
     def prepare(c=conn):
-        # test(c)
+        test(c)
         commit(c)
         push(c)
